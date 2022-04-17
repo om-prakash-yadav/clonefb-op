9e6b7a77192aefb8553d0659d82e047890e4cf4e
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-04-18 02:18:26 +0530

    ui

--- components/Post.js
+++ components/Post.js
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import {ThumbUpIcon, ShareIcon, ChatAltIcon} from "@heroicons/react/outline"
+import { useState } from "react/cjs/react.development";
+
+
 
 function Post({ name, message, postImage, image, timestamp }) {
+  const [isActive, setActive] = useState(false);
+
+  const toggleClass = () => {
+    setActive(!isActive);
+  };
   return (
     <div className="flex flex-col">
       <div className="p-5 bg-white mt-2 md:mt-5 md:rounded-t-2xl shadow-sm">
@@ -35,18 +43,19 @@ function Post({ name, message, postImage, image, timestamp }) {
       {/* footer */}
 
       <div className="flex justify-between items-center md:rounded-b-2xl bg-white shadow-md text-gray-400 border-t">
-        <div className="inputIcon rounded-none md:rounded-bl-2xl ">
-          <div>
-            <ThumbUpIcon className="h-5 md:h-6 text-blue-500" />
-            <p className="flex text-xs justify-center items-center sm:text-base">
-              Like
+        <div  onClick={toggleClass}  className="inputIcon rounded-none md:rounded-bl-2xl">
+          <div className="flex ">
+            <ThumbUpIcon   className={isActive ? "h-5 md:h-6 fill-blue-400 text-blue-600": "h-5 md:h-6 text-blue-600   mr-2"} 
+      />
+            <p className="flex  text-xs justify-center items-center sm:text-base">
+              {isActive ? "You Liked":"Like"}
             </p>
           </div>
         </div>
 
         <div className="inputIcon rounded-none ">
-          <div>
-            <ChatAltIcon className="h-5 md:h-6 text-gray-500"/>
+          <div className="flex">
+            <ChatAltIcon className="h-5 md:h-6 text-gray-500 mr-2"/>
             <p className="flex text-xs justify-center items-center sm:text-base">
               Comment
             </p>
@@ -54,8 +63,8 @@ function Post({ name, message, postImage, image, timestamp }) {
         </div>
 
         <div className="inputIcon rounded-none md:rounded-br-2xl">
-          <div>
-            <ShareIcon className="h-5 md:h-6 text-gray-500"/>
+          <div className="flex">
+            <ShareIcon className="h-5 md:h-6 text-gray-500 mr-2"/>
             <p className="flex text-xs justify-center items-center sm:text-base">
               Share
             </p>
